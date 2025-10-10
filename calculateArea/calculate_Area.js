69a6cda5d4ccba3fed290c6529eea5e5db52a3a9
jlgondin <jeanloup.gondin@orange.com>
2025-10-10 05:44:57 -0400

    calculateArea

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -9,3 +9,17 @@ function calculateArea() {
 
   document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
+
+
+let grocery_1;
+let grocery_2;
+let grocery_3;
+function groceryTracker() {
+    grocery_1 = parseFloat(document.getElementById('grocery1').value);
+    grocery_2 = parseFloat(document.getElementById('grocery2').value);
+    grocery_3 = parseFloat(document.getElementById('grocery3').value);
+
+    let montant_total = grocery_1 + grocery_2 + grocery_3;
+
+    document.getElementById('result_grocery').innerText = `Le montant total est : ${montant_total} euros`;
+}
