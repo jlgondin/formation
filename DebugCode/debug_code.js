18795f998a5d975c1e188d09de88ff6bb1842ec7
jlgondin <jeanloup.gondin@orange.com>
2025-10-14 05:42:02 -0400

    DebugCode

--- DebugCode/debug_code.js
+++ DebugCode/debug_code.js
@@ -6,9 +6,18 @@ function performOperation() {
     if (!isNaN(num1) && !isNaN(num2)) {
         // Perform the operation
         let result = multiply(num1, num2);
-
+        
         // Display the result
         displayResult(result);
+
+        
+        // Perform the operation
+        let result_addition = additionner(num1, num2);
+        
+        // Display the result
+        displayResultAddition(result_addition);
+
+
     } else {
             displayResult('Please enter valid numbers');
     }
@@ -22,8 +31,22 @@ function multiply(a, b) {
     return a * b;
 }
 
+function additionner(a, b) {
+    // Introduce a debugger statement to pause execution
+    debugger;
+
+    // Multiply the numbers
+    return a + b;
+}
+
 function displayResult(result) {
     // Display the result in the paragraph element
     const resultElement = document.getElementById('result');
-    resultElement.textContent = `The result is: ${result}`;
+    resultElement.textContent = `Résultat multiplication: ${result}`;
+}
+
+function displayResultAddition(result_addition) {
+    // Display the result in the paragraph element
+    const resultElement = document.getElementById('result');
+    resultElement.textContent += ` et Résultat addition: ${result_addition}`;
 }
